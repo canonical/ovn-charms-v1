42cd1f2d81dac5d888f3097ec6b1ede31ab64a0b
Frode Nordahl <frode.nordahl@canonical.com>
2020-05-01 21:20:21 +0200

    Consume upstreamed shared OVN library code
    Update charm README
    
    Ignore unsupported configuration options from ``layer-ovn`` and
    provide our own configuration adapter.
    
    At this point in time the DPDK, Hardware offload and SR-IOV
    configuration options and support from ``layer-ovn`` is not
    relevant for this charm.
    
    Update repo info.
    
    Func-Test-Pr: https://github.com/openstack-charmers/zaza-openstack-tests/pull/263
    Change-Id: Ifd65abbd898f735683f6a06c0babcc34ea90ef63

--- ovn-dedicated-chassis/src/lib/charm/openstack/ovn_dedicated_chassis.py
+++ ovn-dedicated-chassis/src/lib/charm/openstack/ovn_dedicated_chassis.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
+import charms_openstack.adapters
 import charms_openstack.charm as charm
 
 import charms.ovn_charm
@@ -20,12 +23,49 @@ import charms.ovn_charm
 charm.use_defaults('charm.default-select-release')
 
 
+# NOTE(fnordahl): We should split the ``OVNConfigurationAdapter`` in
+# ``layer-ovn`` into common and chassis specific parts so we can re-use the
+# common parts here.
+class OVNDedicatedChassisConfigurationAdapter(
+        charms_openstack.adapters.ConfigurationAdapter):
+    """Provide a configuration adapter for OVN."""
+
+    # The charm class initializer will look for these but they are not and will
+    # not be in our config for the time being.
+    enable_dpdk = False
+    enable_sriov = False
+    enable_hardware_offload = False
+
+    @property
+    def ovn_key(self):
+        return os.path.join(self.charm_instance.ovn_sysconfdir(), 'key_host')
+
+    @property
+    def ovn_cert(self):
+        return os.path.join(self.charm_instance.ovn_sysconfdir(), 'cert_host')
+
+    @property
+    def ovn_ca_cert(self):
+        return os.path.join(self.charm_instance.ovn_sysconfdir(),
+                            '{}.crt'.format(self.charm_instance.name))
+
+    @property
+    def chassis_name(self):
+        return self.charm_instance.get_ovs_hostname()
+
+
 class TrainOVNChassisCharm(charms.ovn_charm.BaseTrainOVNChassisCharm):
     # OpenvSwitch and OVN is distributed as part of the Ubuntu Cloud Archive
     # Pockets get their name from OpenStack releases
     source_config_key = 'source'
     release = 'train'
     name = 'ovn-dedicated-chassis'
+    configuration_class = OVNDedicatedChassisConfigurationAdapter
+
+    # NOTE(fnordahl): Add this to ``layer-ovn``
+    def install(self):
+        self.configure_source()
+        super().install()
 
 
 class UssuriOVNChassisCharm(charms.ovn_charm.BaseUssuriOVNChassisCharm):
@@ -34,3 +74,9 @@ class UssuriOVNChassisCharm(charms.ovn_charm.BaseUssuriOVNChassisCharm):
     source_config_key = 'source'
     release = 'ussuri'
     name = 'ovn-dedicated-chassis'
+    configuration_class = OVNDedicatedChassisConfigurationAdapter
+
+    # NOTE(fnordahl): Add this to ``layer-ovn``
+    def install(self):
+        self.configure_source()
+        super().install()
