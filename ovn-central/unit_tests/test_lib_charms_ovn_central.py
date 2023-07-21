f00222ce50203bf5f366ac1855cfd12d2154975d
Martin Kalcok <martin.kalcok@canonical.com>
2023-07-21 09:54:53 +0200

    Don't mask OVN service during charm upgrade.
    Closes-Bug: #2007847
    Change-Id: Icdd8502fa4c176ffc8cc8a0446dd150048d78dd1
    (cherry picked from commit 48cd8f0863c5ccfc1f892355690cb9b3db8e8b3f)

--- ovn-central/unit_tests/test_lib_charms_ovn_central.py
+++ ovn-central/unit_tests/test_lib_charms_ovn_central.py
@@ -128,6 +128,8 @@ class TestOVNCentralCharm(Helper):
         self.patch_object(ovn_central.os, 'symlink')
         self.patch_target('configure_sources')
         self.patch_object(ovn_central.os, 'mkdir')
+        self.patch_object(ovn_central.reactive, 'is_flag_set')
+        self.is_flag_set.return_value = False
         self.target.install()
         calls = []
         for service in (self.target.services[0],
@@ -143,6 +145,31 @@ class TestOVNCentralCharm(Helper):
                 mock.call('/dev/null',
                           '/etc/systemd/system/{}.service'.format(service)))
         self.symlink.assert_has_calls(calls)
+        self.install.assert_called_once_with()
+        self.configure_sources.assert_called_once_with()
+
+    def test_install_during_upgrade(self):
+        """Test that services are not masked during charm upgrade.
+
+        install() handler is also called during charm-upgrade handling
+        and in such case, services should not be masked. Otherwise, it
+        results in upgrade failures.
+        """
+        self.patch_object(ovn_central.charms_openstack.charm.OpenStackCharm,
+                          'install')
+        self.patch_object(ovn_central.os.path, 'islink')
+        self.islink.return_value = False
+        self.patch_object(ovn_central.os, 'symlink')
+        self.patch_target('configure_sources')
+        self.patch_object(ovn_central.os, 'mkdir')
+        self.patch_object(ovn_central.reactive, 'is_flag_set')
+        self.is_flag_set.return_value = True
+
+        self.target.install()
+
+        # Assert that services were not masked
+        self.islink.assert_not_called()
+        self.symlink.assert_not_called()
         self.install.assert_called_once_with()
         self.configure_sources.assert_called_once_with()
 
