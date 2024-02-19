canonical/charm-ovn-chassis

Add ovn cert nrpe check
Certs are root readable so we use a cron job to perform
the check and save state for an nrpe check to read and
send back to nagios.

Closes-Bug: #1979539
Change-Id: Ia6df3fc2e3bc0d64ef2128ed65959a5d2d9b3d62

src/lib/charm/openstack/ovn_chassis.py
@@ -12,9 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
+from charmhelpers.core.host import rsync, write_file
+import charmhelpers.contrib.charmsupport.nrpe as nrpe
+import charmhelpers.fetch as ch_fetch
+
 import charms_openstack.charm as charm
 
 import charms.ovn_charm
+
+NAGIOS_PLUGINS = '/usr/local/lib/nagios/plugins'
+SCRIPTS_DIR = '/usr/local/bin'
+CERTCHECK_CRONFILE = '/etc/cron.d/ovn-central-cert-checks'
+CRONJOB_CMD = "{schedule} root {command} 2>&1 | logger -p local0.notice\n"
 
 
 charm.use_defaults('charm.default-select-release')
@@ -33,3 +44,34 @@ class OVNChassisCharm(charms.ovn_charm.DeferredEventMixin,
     # Setting an empty source_config_key activates special handling of release
     # selection suitable for subordinate charms
     source_config_key = ''
+
+    def render_nrpe(self):
+        hostname = nrpe.get_nagios_hostname()
+        current_unit = nrpe.get_nagios_unit_name()
+        charm_nrpe = nrpe.NRPE(hostname=hostname)
+        self.add_nrpe_certs_check(charm_nrpe)
+        nrpe.add_init_service_checks(
+            charm_nrpe, self.nrpe_check_services, current_unit)
+        charm_nrpe.write()
+
+    def add_nrpe_certs_check(self, charm_nrpe):
+        script = 'nrpe_check_ovn_certs.py'
+        src = os.path.join(os.getenv('CHARM_DIR'), 'files', 'nagios', script)
+        dst = os.path.join(NAGIOS_PLUGINS, script)
+        rsync(src, dst)
+        charm_nrpe.add_check(
+            shortname='check_ovn_certs',
+            description='Check that ovn certs are valid.',
+            check_cmd=script
+        )
+        # Need to install this as a system package since it is needed by the
+        # cron script that runs outside of the charm.
+        ch_fetch.apt_install(['python3-cryptography'])
+        script = 'check_ovn_certs.py'
+        src = os.path.join(os.getenv('CHARM_DIR'), 'files', 'scripts', script)
+        dst = os.path.join(SCRIPTS_DIR, script)
+        rsync(src, dst)
+        cronjob = CRONJOB_CMD.format(
+            schedule='*/5 * * * *',
+            command=dst)
+        write_file(CERTCHECK_CRONFILE, cronjob)

canonical/ovn-charms-v1

(cherry picked from commit 982990bbffe6b6c7609aa7ed69048b3c2eb01e71)
(cherry picked from commit 9c670651d81c50ede859f26ca0547497be0cba3b)
(cherry picked from commit f64ca7dac00db70a091a36c0a1e45d51a8e01d17)

ovn-chassis/src/lib/charm/openstack/ovn_chassis.py
@@ -12,9 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
+from charmhelpers.core.host import rsync, write_file
+from charmhelpers.contrib.charmsupport import nrpe
+import charmhelpers.fetch as ch_fetch
+
 import charms_openstack.charm as charm
 
 import charms.ovn_charm
+
+NAGIOS_PLUGINS = '/usr/local/lib/nagios/plugins'
+SCRIPTS_DIR = '/usr/local/bin'
+CERTCHECK_CRONFILE = '/etc/cron.d/ovn-chassis-cert-checks'
+CRONJOB_CMD = "{schedule} root {command} 2>&1 | logger -p local0.notice\n"
 
 
 charm.use_defaults('charm.default-select-release')
@@ -30,6 +41,40 @@ class OVNChassisCharm(charms.ovn_charm.DeferredEventMixin,
     release = 'ussuri'
     name = 'ovn-chassis'
 
+    # packages needed by nrpe checks
+    nrpe_packages = ['python3-cryptography']
+
     # Setting an empty source_config_key activates special handling of release
     # selection suitable for subordinate charms
     source_config_key = ''
+
+    @property
+    def packages(self):
+        return super().packages + self.nrpe_packages
+
+    def render_nrpe(self):
+        hostname = nrpe.get_nagios_hostname()
+        self.add_nrpe_certs_check(nrpe.NRPE(hostname=hostname))
+        super().render_nrpe()
+
+    def add_nrpe_certs_check(self, charm_nrpe):
+        script = 'nrpe_check_ovn_certs.py'
+        src = os.path.join(os.getenv('CHARM_DIR'), 'files', 'nagios', script)
+        dst = os.path.join(NAGIOS_PLUGINS, script)
+        rsync(src, dst)
+        charm_nrpe.add_check(
+            shortname='check_ovn_certs',
+            description='Check that ovn certs are valid.',
+            check_cmd=script
+        )
+        # Need to install this as a system package since it is needed by the
+        # cron script that runs outside of the charm.
+        ch_fetch.apt_install(['python3-cryptography'])
+        script = 'check_ovn_certs.py'
+        src = os.path.join(os.getenv('CHARM_DIR'), 'files', 'scripts', script)
+        dst = os.path.join(SCRIPTS_DIR, script)
+        rsync(src, dst)
+        cronjob = CRONJOB_CMD.format(
+            schedule='*/15 * * * *',
+            command=dst)
+        write_file(CERTCHECK_CRONFILE, cronjob)